4a5274d63ddcbb4caa6c17a6949e6c8e9cb9dd08
Larbi <ferhaoui.20044@gmail.com>
2020-11-16 19:37:00 +0100

    Edit with Started

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
+import Contrais from './components/contrais/contrais'
+import Details from './components/details/details'
+import Map from './components/map/map'
+
 const App = () => {
+
+    // document.title = "covid 19"
+
     return (
-        <h1>App</h1>
+        <React.Fragment>
+            <h1>App</h1>
+            <Contrais />
+            <Details />
+            <Map />
+        </React.Fragment>
     )
 }
 
