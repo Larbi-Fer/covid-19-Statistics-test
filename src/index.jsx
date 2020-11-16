16e1742e033d24e2df711c4c98e55208bd3ef0d3
Larbi <ferhaoui.20044@gmail.com>
2020-11-16 18:23:21 +0100

    Editing

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Loader, LoaderOptions} from 'google-maps';
+// import {Loader, LoaderOptions} from 'google-maps';
 // import { GoogleMap } from '@react-google-maps/api';
 // import MyComponent from './component/map'
 import App from './App';
