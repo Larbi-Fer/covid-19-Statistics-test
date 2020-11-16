2254d97ee0afcbc3ac5ee7a07047d10a56592179
Larbi <ferhaoui.20044@gmail.com>
2020-11-16 18:16:01 +0100

    Create Project covid-19

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Loader, LoaderOptions} from 'google-maps';
+// import { GoogleMap } from '@react-google-maps/api';
+// import MyComponent from './component/map'
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
